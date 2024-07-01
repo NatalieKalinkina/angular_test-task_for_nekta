9db48ea7fee6af8c9729c6d25bfb02220483e597
Natalie Kalinkina <ms.nataly.r@yandex.ru>
2024-07-01 19:11:46 +0400

    feat: add main components and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { AuthorizationComponent } from './authorization/authorization.component';
+import { DevicesListComponent } from './devices-list/devices-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'auth', component: AuthorizationComponent },
+  { path: 'devices', component: DevicesListComponent },
+];
